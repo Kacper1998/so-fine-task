41dcd642eba9baf53a9639db42f345ca05720edd
Kacper Radzik <KacperRadzik@op.pl>
2023-01-26 10:40:33 +0100

    new slider -change

---
+++ index.js
@@ -0,0 +1,80 @@
+var slideIndex = 0;
+
+showSlide();
+
+// var timer = setInterval(showSlide, 2000);
+
+var slideContainer = document.getElementById('slideshow-container');
+var slideControl = document.getElementById('slide-control');
+
+slideContainer.onmouseover = function() {
+	clearInterval(timer);
+}
+
+slideControl.onmouseover = function() {
+	clearInterval(timer);
+}
+
+slideContainer.onmouseleave = function() {
+	// timer = setInterval(showSlide, 2000);
+}
+
+slideControl.onmouseleave = function() {
+	// timer = setInterval(showSlide, 2000);
+}
+
+function showSlide() {
+	var i;
+	var slides = document.getElementsByClassName('slide-item');
+	var dots = document.getElementsByClassName('dot');
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) slideIndex = 1;
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+}
+
+function chooseSlide(n) {
+	slideIndex = n;
+	showSlide();
+	clearInterval(timer);
+}
+
+function showMenu() {
+	var topNav = document.getElementById('topnav');
+	if (topNav.className === "navbar") {
+		topNav.className += " show";
+	} else {
+		topNav.className = "navbar";
+	}
+}
+
+/*dropdown menu */ 
+
+const dropdownBtn = document.getElementById("btn");
+const dropdownMenu = document.getElementById("dropdown");
+const toggleArrow = document.getElementById("arrow");
+
+// Toggle dropdown function
+const toggleDropdown = function () {
+  dropdownMenu.classList.toggle("show");
+  toggleArrow.classList.toggle("arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", function () {
+  if (dropdownMenu.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
